de7abcbe7c83bd7cb1e9d04f0f23c8d7647aedca
Conor Sullivan <sullivan.conor@yahoo.com>
2014-02-23 15:15:28 -0800

    Updated Settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'conor',
+        'PASSWORD': 'bob',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -67,7 +71,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
